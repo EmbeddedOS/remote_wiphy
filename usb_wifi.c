1b15d48c5c2f471fcfd423bb915e5666b26cdf36
conguyen <conguyen@axon.com>
2026-01-25 16:05:44 +0700

    wip

--- usb_wifi.c
+++ usb_wifi.c
@@ -6,24 +6,58 @@
 #include <linux/module.h>
 #include <linux/sched.h>
 
+static int wifi_bind(struct usb_composite_dev *cdev);
+static int wifi_unbind(struct usb_composite_dev *cdev);
 
 static struct usb_device_descriptor device_desc = {
     .bLength            = sizeof(device_desc),
 };
 
-static int __init usb_wifi_init(void)
-{
+/* Human readable descriptions that will exposed to the host. */
+static struct usb_string strings_dev[] = {
+    [USB_GADGET_MANUFACTURER_IDX].s = "",
+    [USB_GADGET_PRODUCT_IDX].s = "WiFi Adapter",
+    [USB_GADGET_SERIAL_IDX].s = "",
+    {  } /* end of list */
+};
 
-    return 0;
+static struct usb_gadget_strings stringtab_dev = {
+    .language = 0x0409,  /* en-us */
+    .strings  = strings_dev,
+};
+
+static struct usb_gadget_strings *dev_strings[] = {
+    &stringtab_dev,
+    NULL,
+};
+
+static struct usb_composite_driver wifi_driver = {
+    .name       = "WiFi Adapter",
+    .dev        = &device_desc,
+    .strings    = dev_strings,
+    .max_speed  = USB_SPEED_SUPER,
+    .bind       = wifi_bind,
+    .unbind     = wifi_unbind,
+};
+
+static int wifi_bind(struct usb_composite_dev *cdev)
+{ // Called when the USB cable is connected.
+    int ret = 0;
+    pr_info("Bind\n");
+
+    return ret;
 }
 
-static void __exit usb_wifi_exit(void)
-{
+static int wifi_unbind(struct usb_composite_dev *cdev)
+{ // Called when the USB cable is disconnected.
+    int ret = 0;
+    pr_info("Unbind\n");
 
+    return ret;
 }
 
-module_init(usb_wifi_init);
-module_exit(usb_wifi_exit);
+
+module_usb_composite_driver(wifi_driver);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("EmbeddedOS");
